03c714b557b42ad0464388c161a04e12e46afadc
joaoccmm <87348459+joaoccmm@users.noreply.github.com>
2024-03-13 22:10:13 -0300

    Add files via upload

--- src/Components/Weather/index.jsx
+++ src/Components/Weather/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 //assets
+import humidityIcon from '../Assets/humidity.png';
 import drizzleIcon from '../Assets/drizzle.png';
-import humidity from '../Assets/humidity.png';
 import searchIcon from '../Assets/search.png';
 import clearicon from '../Assets/clear.png';
 import cloudIcon from '../Assets/cloud.png';
@@ -11,29 +11,94 @@ import windIcon from '../Assets/wind.png';
 //styles
 import './styles.css';
 
-export const Weather = () => {
+export const Weather = () => {  
+  const [cityName, setCityName] = useState('Belo Horizonte');
+  const [weatherData, setWeatherData] = useState(null);
+  const [weatherIcon, setWeatherIcon] = useState();
+
+  const weatherIconMapping = {
+    '01d': clearicon,
+    '02d': cloudIcon,
+    '03d': drizzleIcon,
+    '04d': drizzleIcon,
+    '09d': rainIcon,
+    '10d': rainIcon,
+    '11d': snowIcon,
+    '13d': snowIcon
+  };
+
+  const handleWeatherIcon = () => {
+    if(weatherData?.weather && weatherData.weather[0]?.icon) {
+      const iconCode = weatherData.weather[0].icon;
+      const icon = weatherIconMapping[iconCode] || clearicon;
+      setWeatherIcon(icon);
+    }
+  }
+
+  const searchLocation = async (city) => {
+    if (city === "") {
+      return;
+    }
+
+    const apiKey = "719dbf91b3fb39ce60ff81ba1e54c1c1";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setWeatherData(data);
+      setCityName("")
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  }
+
+  useEffect(() => {
+    searchLocation(cityName)
+  }, []);
+
+  useEffect(() => {
+    handleWeatherIcon();
+  }, [weatherData]);
+
   return (
     <div className='container'>
       <div className="topBar">
-        <input type="text" className="cityInput" placeholder='Search'/>
-        <div className="searchIcon">
+
+        <input 
+        type="text" 
+        value={cityName} 
+        placeholder='Search' 
+        className="cityInput" 
+        onChange={(e) => setCityName(e.target.value)}
+        // onKeyPress={}
+        />
+
+        <div className="searchIcon" onClick={() => {searchLocation(cityName)}}>
           <img src={searchIcon} alt="search icon" />
         </div>
+
       </div>
       <div className="weatherIcon">
-        <img src={cloudIcon} alt="Weather Icon" />
+       <img src={weatherIcon} alt="Weather Icon" />
       </div>
-      <div className="weatherTemp">24ºC</div>
-      <div className="weatherLocation">NY</div>
-      <div className="dataCotainer">
+      <div className="weatherTemp">{Math.floor(weatherData?.main?.temp)} °C</div>
+      <div className="weatherLocation">{weatherData?.name}</div>
+      <div className="dataContainer">
         <div className="element">
-          <img src="" alt="" className="iconElement" />
+          <img src={humidityIcon} alt="" className="iconElement" />
           <div className="data">
-            <div className="humidityPercent">
-              
-            </div>
+            <div className="humidityPercent">{Math.floor(weatherData?.main?.humidity)} %</div>
+            <div className="text">Humidity</div>
           </div>
         </div>
+        <div className="element">
+          <img src={windIcon} alt="" className="iconElement" />
+          <div className="data">
+            <div className="humidityPercent">{Math.floor(weatherData?.wind?.speed)} Km/h</div>
+            <div className="text">Wind Speed</div>
+          </div>
+        </div>  
       </div>
     </div>
   );
